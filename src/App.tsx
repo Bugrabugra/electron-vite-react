f41c613a92e5d8de41a271752ee24c3c3ab1e74e
Bugrabugra <bugraotken@gmail.com>
2022-06-28 20:11:36 +0300

    added eslint and prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import React, { useEffect, useState } from 'react'
-import styles from 'styles/app.module.scss'
+import React, { useEffect, useState } from "react";
+import styles from "styles/app.module.scss";
 
 interface User {
-  id: number
-  name: string,
+  id: number;
+  name: string;
   address: {
-    street: string,
-    suite: string
-  }
+    street: string;
+    suite: string;
+  };
 }
 
 const App: React.FC = () => {
@@ -15,7 +15,9 @@ const App: React.FC = () => {
 
   useEffect(() => {
     const getUsers = async () => {
-      const response = await fetch("https://jsonplaceholder.typicode.com/users");
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/users"
+      );
       const json = await response.json();
 
       setUsers(json);
@@ -24,15 +26,16 @@ const App: React.FC = () => {
     getUsers();
   }, []);
 
-
   return (
     <div className={styles.app}>
-      {users && users.map(user => (
-        <p key={user.id}>
-          {user.name} - {user.address.street}
-        </p>))}
+      {users &&
+        users.map((user) => (
+          <p key={user.id}>
+            {user.name} - {user.address.street}
+          </p>
+        ))}
     </div>
-  )
-}
+  );
+};
 
 export default App;
